41d078d12a2827ac06b1b341b3380ff721eb4e50
Troy Chryssos <github email address>
2016-08-24 15:54:16 -0400

    Used partials to render views, and created student search Used partials _student and _form to make rendering student and classroom views easier. Also created a Student.search method which returns all students whose name begins with the argument string. The index page of our app then returns those students.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,12 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+  def self.search(student_name)
+    if student_name==""
+      Student.all
+    else
+      self.where("name LIKE ?","#{student_name}%")
+    end
+  end
 end
